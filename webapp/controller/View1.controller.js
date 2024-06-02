1cb422461f8e8d77b33597a993c0aede23e8af1b
TUSHAR SHUKLA <tusharshukla0907@gmail.com>
2024-06-02 09:42:09 +0000

    second commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,11 +1,120 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/json/JSONModel"
 ],
-function (Controller) {
+function (Controller,JSONModel) {
     "use strict";
 
     return Controller.extend("com.tushar.project1.controller.View1", {
         onInit: function () {
+            var tickets = [ 
+                {
+                "status": "resolved",
+                "ticketid" : 1,
+                "creation date":  "2024-06-01",
+                "createdby": "Tushar",
+                "priority": "high",
+                "assignedto": "girish"
+
+            },
+            {
+                "status": "resolved",
+                "ticket id" : 1,
+                "creationdate":"2024-06-0",
+                "createdby": "Tushar",
+                "priority": "high",
+                "assignedto": "girish"
+
+            },
+            {
+                "status": "resolved",
+                "ticketid" : 1,
+                "creationdate": "2024-06-01",
+                "createdby": "Tushar",
+                "priority": "high",
+                "assignedto": "girish"
+
+            },
+            {
+                "status": "resolved",
+                "ticketid" : 1,
+                "creationdate": "2024-06-01",
+                "createdby": "Tushar",
+                "priority": "high",
+                "assigned to": "girish"
+
+            },
+            {
+                "status": "resolved",
+                "ticketid" : 1,
+                "creationdate": "2024-06-01",
+                "createdby": "Tushar",
+                "priority": "high",
+                "assignedto": "girish"
+
+            },
+            {
+                "status": "resolved",
+                "ticketid" : 1,
+                "creation date": "2024-06-01",
+                "createdby": "Tushar",
+                "priority": "high",
+                "assignedto": "girish"
+
+            },
+            {
+                "status": "resolved",
+                "ticketid" : 1,
+                "creationdate": "2024-06-01",
+                "createdby": "Tushar",
+                "priority": "high",
+                "assignedto": "girish"
+
+            },
+            {
+                "status": "resolved",
+                "ticketid" : 1,
+                "creationdate": "2024-06-01",
+                "createdby": "Tushar",
+                "priority": "high",
+                "assignedto": "girish"
+
+            },
+            {
+                "status": "resolved",
+                "ticketid" : 1,
+                "creationdate": "2024-06-01",
+                "createdby": "Tushar",
+                "priority": "high",
+                "assignedto": "girish"
+
+            },
+            {
+                "status": "resolved",
+                "ticketid" : 1,
+                "creationdate": "2024-06-01",
+                "createdby": "Tushar",
+                "priority": "high",
+                "assignedto": "girish"
+
+            },
+            {
+                "status": "resolved",
+                "ticketid" : 1,
+                "creationdate": "2024-06-01",
+                "created by": "Tushar",
+                "priority": "high",
+                "assignedto": "girish"
+
+            }
+
+            ]
+            
+            let oTicket = new JSONModel(); 
+            oTicket.setData(tickets); 
+            this.getView().byId("table").setModel(oTicket,"T");
+
+            
 
         }
     });

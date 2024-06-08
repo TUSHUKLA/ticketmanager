521a91cd95f582c248a0aee612047053d43575f1
TUSHAR SHUKLA <tusharshukla0907@gmail.com>
2024-06-08 12:18:48 +0000

    new changes

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,129 +1,245 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/ui/model/json/JSONModel",
-    "sap/m/MessageToast"
+    "sap/m/MessageToast",
+    "sap/ui/core/Fragment",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
 ],
-function (Controller,JSONModel, MessageToast) {
+function (Controller,JSONModel, MessageToast,Fragment,Filter,FilterOperator) {
     "use strict";
 
     return Controller.extend("com.tushar.project1.controller.View1", {
         onInit: function () {
-            var tickets = [ 
+            const tickets = [ 
                 {
                 "status": "resolved",
                 "ticketid" : "0001",
-                "creation date":  "2024-06-01",
-                "createdby": "Tushar",
+                "creationdate":  "2024-06-01",
+                "createdby": "Girish",
                 "priority": "high",
-                "assignedto": "girish"
+                "assignedto": "Cofforge"
 
             },
             {
                 "status": "resolved",
-                "ticket id" : "0001",
-                "creationdate":"2024-06-0",
-                "createdby": "Tushar",
-                "priority": "high",
-                "assignedto": "girish"
+                "ticketid" : "0002",
+                "creationdate":"2024-06-05",
+                "createdby": "Manish",
+                "priority": "medium",
+                "assignedto": "Bluejay"
 
             },
             {
                 "status": "resolved",
-                "ticketid" : "0001",
+                "ticketid" : "0003",
+                "creationdate": "2024-06-01",
+                "createdby": "Parnika",
+                "priority": " verry high",
+                "assignedto": "Aricord"
+
+            },
+            {
+                "status": "resolved",
+                "ticketid" : "0004",
+                "creationdate": "2024-06-08",
+                "createdby": "Ajay",
+                "priority": "medium",
+                "assignedto": "Persistant"
+
+            },
+            {
+                "status": "resolved",
+                "ticketid" : "0005",
+                "creationdate": "2024-06-10",
+                "createdby": "Tushar",
+                "priority": "high",
+                "assignedto": "Aricord"
+
+            },
+            {
+                "status": "resolved",
+                "ticketid" : "0006",
+                "creationdate": "2024-06-10",
+                "createdby": "Manish",
+                "priority": "low",
+                "assignedto": "Persistant"
+
+            },
+            {
+                "status": "resolved",
+                "ticketid" : "0007",
                 "creationdate": "2024-06-01",
                 "createdby": "Tushar",
-                "priority": "high",
-                "assignedto": "girish"
+                "priority": "medium",
+                "assignedto": "Cofforge"
 
             },
             {
                 "status": "resolved",
-                "ticketid" : "0001",
+                "ticketid" : "0008",
                 "creationdate": "2024-06-01",
-                "createdby": "Tushar",
-                "priority": "high",
-                "assigned to": "girish"
+                "createdby": "Ajay",
+                "priority": "verry high",
+                "assignedto": "TCS"
 
             },
             {
                 "status": "resolved",
-                "ticketid" : "0001",
+                "ticketid" : "0009",
                 "creationdate": "2024-06-01",
-                "createdby": "Tushar",
-                "priority": "high",
-                "assignedto": "girish"
+                "createdby": "Manish",
+                "priority": "verry low",
+                "assignedto": "TCS"
 
             },
             {
                 "status": "resolved",
-                "ticketid" : "0001",
-                "creation date": "2024-06-01",
-                "createdby": "Tushar",
-                "priority": "high",
-                "assignedto": "girish"
-
-            },
-            {
-                "status": "resolved",
-                "ticketid" : "0001",
+                "ticketid" : "0010",
                 "creationdate": "2024-06-01",
-                "createdby": "Tushar",
+                "createdby": "Parnika",
                 "priority": "high",
-                "assignedto": "girish"
-
-            },
-            {
-                "status": "resolved",
-                "ticketid" : "0001",
-                "creationdate": "2024-06-01",
-                "createdby": "Tushar",
-                "priority": "high",
-                "assignedto": "girish"
-
-            },
-            {
-                "status": "resolved",
-                "ticketid" : "0001",
-                "creationdate": "2024-06-01",
-                "createdby": "Tushar",
-                "priority": "high",
-                "assignedto": "girish"
-
-            },
-            {
-                "status": "resolved",
-                "ticketid" : "0001",
-                "creationdate": "2024-06-01",
-                "createdby": "Tushar",
-                "priority": "high",
-                "assignedto": "girish"
-
-            },
-            {
-                "status": "resolved",
-                "ticketid" : "0001",
-                "creationdate": "2024-06-01",
-                "created by": "Tushar",
-                "priority": "high",
-                "assignedto": "girish"
+                "assignedto": "Bluejay"
 
             }
-
+            
             ]
             
-            let oTicket = new JSONModel(); 
+            const oTicket = new JSONModel(); 
             oTicket.setData(tickets); 
             this.getView().byId("table").setModel(oTicket,"T");            
         },
 
-        onSearch:function(){
-            let s= this.query().byId("SF");
-            let page=this.getview().byid("ObjectPageLayout");
-            if(s==page)
+        onValueHelpRequestid: function (oEvent) {
+			var sInputValue = oEvent.getSource().getValue(),
+				oView = this.getView();
+
+			if (!this._psuggest) {
+				this._psuggest = Fragment.load({
+					id: oView.getId(),
+					name: "com.tushar.project1.view.suggest",
+					controller: this
+				}).then(function (oDialog) {
+					oView.addDependent(oDialog);
+					return oDialog;
+				});
+			}
+			this._psuggest.then(function(oDialog) {
+				// Create a filter for the binding
+				oDialog.getBinding("items").filter([new Filter("Name", FilterOperator.Contains, sInputValue)]);
+				// Open ValueHelpDialog filtered by the input's value
+				oDialog.open(sInputValue);
+			});
+		},
+
+		onValueHelpSearch: function (oEvent) 
+        {
+			var sValue = oEvent.getParameter("value");
+			var oFilter = new Filter("Name", FilterOperator.Contains, sValue);
+
+			oEvent.getSource().getBinding("items").filter([oFilter]);
+		},
+
+		onValueHelpClose: function (oEvent) {
+			var oSelectedItem = oEvent.getParameter("selectedItem");
+			oEvent.getSource().getBinding("items").filter([]);
+
+			if (!oSelectedItem) {
+				return;
+			}
+
+			this.byId("TI").setValue(oSelectedItem.getTitle());
+
+       
+        },
+
+        onValueHelpRequestassign: function (oEvent) {
+			var sInputValue2 = oEvent.getSource().getValue(),
+				oView = this.getView();
+
+			if (!this._psuggestassign) {
+				this._psuggestassign = Fragment.load({
+					id: oView.getId(),
+					name: "com.tushar.project1.view.suggestassign",
+					controller: this
+				}).then(function (oDialog) {
+					oView.addDependent(oDialog);
+					return oDialog;
+				});
+			}
+			this._psuggestassign.then(function(oDialog) 
             {
+				// Create a filter for the binding
+				oDialog.getBinding("items").filter([new Filter("Name", FilterOperator.Contains, sInputValue2)]);
+				// Open ValueHelpDialog filtered by the input's value
+				oDialog.open(sInputValue2);
+			});
+		},
 
-            }
+		onValueHelpSearch: function (oEvent) {
+			var sValue = oEvent.getParameter("value");
+			var oFilter = new Filter("Name", FilterOperator.Contains, sValue);
 
+			oEvent.getSource().getBinding("items").filter([oFilter]);
+		},
+
+		onValueHelpClose: function (oEvent) {
+			var oSelectedItem = oEvent.getParameter("selectedItem");
+			oEvent.getSource().getBinding("items").filter([]);
+
+			if (!oSelectedItem) {
+				return;
+			}
+
+			this.byId("AT").setValue(oSelectedItem.getTitle());
+
+       
+        }, 
+        
+        onValueHelpRequestcreate: function (oEvent) {
+			var sInputValue1 = oEvent.getSource().getValue(),
+				oView = this.getView();
+
+			if (!this._psuggestcreate) {
+				this._psuggestcreate = Fragment.load({
+					id: oView.getId(),
+					name: "com.tushar.project1.view.suggestcreate",
+					controller: this
+				}).then(function (oDialog) {
+					oView.addDependent(oDialog);
+					return oDialog;
+				});
+			}
+			this._psuggestcreate.then(function(oDialog) 
+            {
+				// Create a filter for the binding
+				oDialog.getBinding("items").filter([new Filter("Name", FilterOperator.Contains, sInputValue1)]);
+				// Open ValueHelpDialog filtered by the input's value
+				oDialog.open(sInputValue1);
+			});
+		},
+
+		onValueHelpSearch: function (oEvent) 
+        {
+			var sValue = oEvent.getParameter("value");
+			var oFilter = new Filter("Name", FilterOperator.Contains, sValue);
+
+			oEvent.getSource().getBinding("items").filter([oFilter]);
+		},
+
+		onValueHelpClose: function (oEvent)
+         {
+			var oSelectedItem = oEvent.getParameter("selectedItem");
+			oEvent.getSource().getBinding("items").filter([]);
+
+			if (!oSelectedItem) 
+            {
+				return;
+			}
+
+			this.byId("CB").setValue(oSelectedItem.getTitle());
+
+       
         }
 
     });
